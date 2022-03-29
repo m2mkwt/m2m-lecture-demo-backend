35d4ab376ca8c1616208efdc01d7e2541c58cdd3
oning <oning1114@gmail.com>
2022-03-29 16:25:28 +0900

    comment 수정

--- src/main/java/kr/co/m2m/instagram/comment/controller/CommentController.java
+++ src/main/java/kr/co/m2m/instagram/comment/controller/CommentController.java
@@ -5,11 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import kr.co.m2m.framework.web.model.BasicResponse;
 import kr.co.m2m.framework.web.model.CommonResponse;
+//import kr.co.m2m.framework.web.model.ErrorResponse;
+import kr.co.m2m.instagram.comment.model.CommentPO;
+import kr.co.m2m.instagram.comment.model.CommentSO;
 import kr.co.m2m.instagram.comment.model.CommentVO;
 import kr.co.m2m.instagram.comment.service.impl.CommentServiceImpl;
 import lombok.extern.slf4j.Slf4j;
@@ -17,59 +23,39 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 @RequestMapping("/comment")
-
+@CrossOrigin("*")
 public class CommentController {
  
 	@Autowired
-    private CommentServiceImpl mCommentService;
-        
+    private CommentServiceImpl commentService;
+	
     @RequestMapping("commentList") //댓글 리스트
     public ResponseEntity<? extends BasicResponse> commentList(CommentVO cv,Model model) {
-    	List<CommentVO> commetList = mCommentService.commentList(cv);
+    	List<CommentVO> commetList = commentService.commentList(cv);
     	model.addAttribute("commentList",commetList);
     	return ResponseEntity.ok().body(new CommonResponse<List<CommentVO>>(commetList));
     }
- 
-//	@RequestMapping("commentInsert")
-//    public ResponseEntity<? extends BasicResponse> commentInsert (CommentPO po) {
-//		log.debug("Input Parameter (PO) : {}", po);
-//		ResultModel<String> result = mCommentService.commentInsert(po);
-//        return ResponseEntity.ok().body(new CommonResponse<ResultModel<String>>(result));
-//    }
-//	@PostMapping(value = "insert")
-//	public ResultModel<String> commentInsert(@RequestBody List<CommentPO> po) {
-//		log.info(" commentInsert Contorller Start..... SO : {}", po);
-//		return mCommentService.commentInsert(po);
-//	}
-// 	@RequestMapping("commentInsert") //댓글 작성
-// 	public String commentInsert(CommentVO cv,Model model) {
-// 		int result = mCommentService.commentInsert(cv);
-//    	return "post/commentList";
-// 	}
-//	@PostMapping(value = "update")
-//	public ResultListModel<CommentVO> commentUpdate(@RequestBody List<CommentVO> vo) {
-//		log.info("Contorller Start..... SO : {}", vo);
-//		return mCommentService.commentUpdate(vo);
-//	}
-	
-	/* 
-	 * @RequestMapping("update") //댓글 수정
-	 * 
-	 * @ResponseBody private int mCommentServiceUpdateProc(@RequestParam int
-	 * cno, @RequestParam String content) throws Exception{
-	 * 
-	 * CommentVO comment = new CommentVO(); comment.setCno(cno);
-	 * comment.setContent(content);
-	 * 
-	 * return mCommentService.commentUpdateService(comment); }
-	 * 
-	 * @RequestMapping("delete/{cno}") //댓글 삭제
-	 * 
-	 * @ResponseBody private int mCommentServiceDelete(@PathVariable int cno) throws
-	 * Exception{
-	 * 
-	 * return mCommentService.commentDeleteService(cno); }
-	 */
+    @PostMapping("select") //댓글 상세보기
+	public ResponseEntity<? extends BasicResponse> selectComment(CommentSO cs,Model model) {
+    	List<CommentSO> selectComment = commentService.selectComment(cs);
+		log.info("Contorller Start..... SO : {}", model);
+		return ResponseEntity.ok().body(new CommonResponse<List<CommentSO>>(selectComment));
+	}
+    
+    @PostMapping("insert")
+	public ResponseEntity<? extends BasicResponse> insertComment(@Valid CommentPO cp,Model model) {
+		String result = commentService.insertComment(cp);
+		return ResponseEntity.ok().body(new CommonResponse<String>(result));
+	}
+    @PostMapping(value = "update")
+	public ResponseEntity<? extends BasicResponse> updateComment(CommentPO cp) {
+		String result = commentService.updateComment(cp);
+		return ResponseEntity.ok().body(new CommonResponse<String>(result));
+	}
+    @PostMapping(value = "delete")
+	public ResponseEntity<? extends BasicResponse> deleteComment(CommentPO cp) {
+		String result = commentService.deleteComment(cp);
+		return ResponseEntity.ok().body(new CommonResponse<String>(result));
+	}
 }
-
    
