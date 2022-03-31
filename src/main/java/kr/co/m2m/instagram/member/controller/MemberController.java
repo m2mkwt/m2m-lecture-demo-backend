19dc428e50e25331e4ba92818448eb03487bffdf
LAPTOP-TMM3HJTP\m2m-128 <qndrl1004@gmail.com>
2022-03-31 14:11:23 +0900

    @회원가입 api 유효성 수정

--- src/main/java/kr/co/m2m/instagram/member/controller/MemberController.java
+++ src/main/java/kr/co/m2m/instagram/member/controller/MemberController.java
@@ -2,6 +2,8 @@ package kr.co.m2m.instagram.member.controller;
 
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -65,14 +67,14 @@ public class MemberController {
 	
     //회원 가입 폼
 	@PostMapping("signup")
-	public String signup(@RequestBody MemberVO memberVO) {
+	public String signup(@Valid@RequestBody MemberVO memberVO) {
 		//패스워드 암호화
 		String enc = passwordEncoder.encode(memberVO.getPassword());
 		memberVO.setPassword(enc);
 		//회원정보 등록
 		memberService.insertMember(memberVO);
 		
-		return "redirect:/member/login";
+		return "success";
 
 	}
 	
