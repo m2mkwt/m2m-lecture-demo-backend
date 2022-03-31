df2cd1bd739aff726a558bc443a5dfd03e382e7e
LAPTOP-TMM3HJTP\m2m-128 <qndrl1004@gmail.com>
2022-03-31 14:07:52 +0900

    회원가입 유효성검사 업데이트 정붕기

--- src/main/java/kr/co/m2m/instagram/member/controller/MemberController.java
+++ src/main/java/kr/co/m2m/instagram/member/controller/MemberController.java
@@ -2,6 +2,8 @@ package kr.co.m2m.instagram.member.controller;
 
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,7 +34,6 @@ public class MemberController {
 
 	@Autowired
 	private PasswordEncoder passwordEncoder;
-	
 
 	// 로그인
 	@RequestMapping("login")
@@ -49,7 +50,7 @@ public class MemberController {
 
 	// 아이디 중복검사
 	@ResponseBody
-	@RequestMapping(value = "idCheck", produces ="application/json")
+	@RequestMapping(value = "idCheck", produces = "application/json")
 	public int idCheck(@RequestBody MemberVO memberVO) {
 		int result = memberService.idCheck(memberVO);
 		return result;
@@ -57,25 +58,25 @@ public class MemberController {
 
 	// 패스워드 검사
 	@ResponseBody
-	@RequestMapping(value = "pwCheck", produces ="application/json")
+	@RequestMapping(value = "pwCheck", produces = "application/json")
 	public int pwCheck(@RequestBody MemberVO memberVO) {
 		int result = SecurityUtil.checkPassword(memberVO.getPassword());
 		return result;
 	}
-	
-    //회원 가입 폼
-	@PostMapping("signup")
-	public String signup(@RequestBody MemberVO memberVO) {
-		//패스워드 암호화
-		String enc = passwordEncoder.encode(memberVO.getPassword());
-		memberVO.setPassword(enc);
-		//회원정보 등록
-		memberService.insertMember(memberVO);
-		
-		return "redirect:/member/login";
 
+	// 회원 가입 폼
+	@PostMapping("signup")
+	public String signup(@Valid@RequestBody MemberVO memberVO) {
+		
+			// 패스워드 암호화
+			String enc = passwordEncoder.encode(memberVO.getPassword());
+			memberVO.setPassword(enc);
+			// 회원정보 등록
+			memberService.insertMember(memberVO);
+
+			return"sucess";
 	}
-	
+
 	// 프로필(회원 정보) 조회
 	@RequestMapping("profile")
 	public MemberVO selectMember(@RequestParam(value = "memberNo") int memberNo) {
@@ -83,7 +84,7 @@ public class MemberController {
 		log.info(mvo.toString());
 		return mvo;
 	}
-	
+
 	// 프로필(회원 정보) 수정
 	@RequestMapping(value = "updateProfile", method = RequestMethod.POST)
 	public ModelAndView updateProfile(@RequestBody MemberVO mvo/* , RedirectAttributes rattr */) {
@@ -95,7 +96,7 @@ public class MemberController {
 //		rattr.addAttribute("memberNo", mvo.getMemberNo());
 		return modelAndView;
 	}
-	
+
 //	@RequestMapping(value = "updateProfile", method = RequestMethod.POST)
 //	public String updateProfile(@RequestBody MemberVO mvo, RedirectAttributes rattr) {
 //		System.out.println(mvo);
@@ -104,12 +105,12 @@ public class MemberController {
 //		rattr.addAttribute("memberNo", mvo.getMemberNo());
 //		return "redirect:profile";
 //	}
-	
+
 	// 회원 비밀번호 수정
 //	@RequestMapping(value = "udpatePassword", method = RequestMethod.POST)
 //	public String updatePassword(@RequestBody Map<String, String> passwordMap) {
 //		
 //		return null;
 //	}
-	
+
 }
