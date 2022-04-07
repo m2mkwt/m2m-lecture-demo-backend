e97ac4430e1f8e5e78eccbab4467b2c179000466
LAPTOP-TMM3HJTP\m2m-128 <qndrl1004@gmail.com>
2022-04-07 16:04:25 +0900

    정붕기 회원가입 주석달기 입니다.

--- src/main/java/kr/co/m2m/instagram/member/mapper/MemberMapper.java
+++ src/main/java/kr/co/m2m/instagram/member/mapper/MemberMapper.java
@@ -1,35 +1,46 @@
 package kr.co.m2m.instagram.member.mapper;
 
-import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
-
 import kr.co.m2m.instagram.common.model.LoginSO;
 import kr.co.m2m.instagram.common.model.LoginVO;
 import kr.co.m2m.instagram.member.model.MemberVO;
 
+/**
+ * <pre>
+ * 프로젝트명	: m2m-lecture-demo-backend
+ * 패키지명		: kr.co.m2m.instagram.member.mapper
+ * 파일명		: MemberMapper.java
+ * 작성일		: 2022-04-07
+ * 작성자		: bkJung
+ * 설명		 	: 회원가입의 매퍼파일입니다.
+ *
+ * 수정내역(수정일 수정자 - 수정내용)
+ * -------------------------------------------------------------------------
+ * 2022-04-07	bkJung - 최초생성
+ * </pre>
+ */
 @Mapper
 public interface MemberMapper {
-	
-	//회원 검색
-    LoginVO getLoginMember(LoginSO so);
-	
-	//회원 가입
-	void insertMember(MemberVO memberVO);
-	
-	//회원정보 수정
-	int updateMember(MemberVO memberVO);
-	
-	//회원정보 조회
-	MemberVO selectMember(int memberNo);
-	
-	//아이디 체크
-	int idCheck(MemberVO memberVO);
-	
-	//비밀번호 수정
-	int updatePassword(MemberVO memberVO);
-	
-	//비밀번호 조회
-	String selectPassword(int memberNo);
-	
+
+  // 회원 검색
+  LoginVO getLoginMember(LoginSO so);
+
+  // 회원 가입
+  void insertMember(MemberVO memberVO);
+
+  // 회원정보 수정
+  int updateMember(MemberVO memberVO);
+
+  // 회원정보 조회
+  MemberVO selectMember(int memberNo);
+
+  // 아이디 체크
+  int idCheck(MemberVO memberVO);
+
+  // 비밀번호 수정
+  int updatePassword(MemberVO memberVO);
+
+  // 비밀번호 조회
+  String selectPassword(int memberNo);
+
 }
