c3f70082225150dfc7c8ba20d764f3c743d2109c
m2m cshwang <hcs88cp@gmail.com>
2022-04-07 15:18:21 +0900

    주석 추가, 포맷 적용

--- src/main/java/kr/co/m2m/instagram/media/service/MediaService.java
+++ src/main/java/kr/co/m2m/instagram/media/service/MediaService.java
@@ -3,21 +3,41 @@ package kr.co.m2m.instagram.media.service;
 import kr.co.m2m.framework.web.model.FileVO;
 import kr.co.m2m.instagram.media.model.MediaVO;
 
+/**
+ * <pre>
+ * 프로젝트명	: m2m-lecture-demo-backend
+ * 패키지명	: kr.co.m2m.instagram.media.service
+ * 파일명		: MediaService.java
+ * 작성일		: 2022-04-07
+ * 작성자		: "cshwang"
+ * 설명		: 게시글/프로필에서 사용되는 이미지 등록/수정을 위한 서비스 인터페이스
+ *
+ * 수정내역(수정일 수정자 - 수정내용)
+ * -------------------------------------------------------------------------
+ * 2022-04-07	"cshwang" - 최초생성
+ * </pre>
+ */
 public interface MediaService {
-	
-	// 이미지 추가 (프로필, 게시글)
-	public FileVO insertProfileMedia(FileVO file);
-	public FileVO insertPostMedia(FileVO file);
-	
-	// 이미지 수정 (프로필, 게시글) (기존 mediaNo에 해당하는 이미지 deleteYn=Y 처리 후, 신규 file insert)
-	public FileVO updateProfileMedia(FileVO file);
-	public FileVO updatePostMedia(FileVO file);
-	
-	// 이미지 삭제 (프로필, 게시글) (리스트 출력되지 않도록 delete_yn 값 'Y' 설정)
-	public void deleteProfileMedia(FileVO file);
-	public void deletePostMedia(FileVO file);
-	
-	// 이미지 조회
-	public MediaVO selectMedia(int mediaNo);
-	
+
+  // 게시글 이미지 추가
+  public FileVO insertProfileMedia(FileVO file);
+
+  // 프로필 이미지 추가
+  public FileVO insertPostMedia(FileVO file);
+
+  // 게시글 이미지 수정
+  public FileVO updateProfileMedia(FileVO file);
+
+  // 프로필 이미지 수정
+  public FileVO updatePostMedia(FileVO file);
+
+  // 게시글 이미지 삭제
+  public void deletePostMedia(FileVO file);
+
+  // 프로필 이미지 삭제
+  public void deleteProfileMedia(FileVO file);
+
+  // 이미지 1건 정보 조회
+  public MediaVO selectMedia(int mediaNo);
+
 }
