5be680ac46eb127615306518461245d70d8434dc
m2m cshwang <hcs88cp@gmail.com>
2022-04-07 17:52:31 +0900

    의존성 버전 관리, 불필요한 코드 삭제

--- src/main/java/kr/co/m2m/instagram/common/controller/CommonController.java
+++ src/main/java/kr/co/m2m/instagram/common/controller/CommonController.java
@@ -2,7 +2,6 @@ package kr.co.m2m.instagram.common.controller;
 
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -16,11 +15,7 @@ import kr.co.m2m.framework.web.model.CommonResponse;
 import kr.co.m2m.framework.web.model.ErrorResponse;
 import kr.co.m2m.framework.web.model.FileVO;
 import kr.co.m2m.instagram.common.service.CommonService;
-import kr.co.m2m.instagram.media.model.MediaVO;
 import kr.co.m2m.instagram.media.service.MediaService;
-import kr.co.m2m.instagram.member.model.MemberVO;
-import kr.co.m2m.instagram.member.service.MemberService;
-import kr.co.m2m.instagram.post.service.PostService;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j // 로그출력에 사용함 (ex. log.debug(String), debug 외에 info, warn 등 사용 가능함)
@@ -30,55 +25,54 @@ public class CommonController {
 
   @Autowired
   private CommonService commonService;
-  
+
   @Autowired
   private MediaService mediaService;
-  
-  @Autowired
-  private MemberService memberService;
-  
-  @Autowired
-  private PostService postService;
-  
+
   @CrossOrigin("*")
   @RequestMapping(value = "uploadImg", method = RequestMethod.POST)
-  public ResponseEntity<? extends BasicResponse> requestUploadImage(@RequestParam("fileList") MultipartFile mFile,
-		  @RequestParam Map<String, Object> map){
+  public ResponseEntity<? extends BasicResponse> requestUploadImage(
+      @RequestParam("fileList") MultipartFile mFile, @RequestParam Map<String, Object> map) {
     FileVO file = null;
     log.info("[uploadImg] mFile : {}", mFile);
     log.info("[upload info] map : {}", map);
     try {
       file = commonService.saveSvrFileData(mFile, "");
       int memberNo = Integer.parseInt(map.get("memberNo").toString());
-	  file.setMemberNo(memberNo);
+      file.setMemberNo(memberNo);
       String uploadType = map.get("uploadType").toString();
       if (uploadType.equals("profile")) {
-          int mediaNo = Integer.parseInt(map.get("mediaNo").toString());
-    	  file.setMediaNo(mediaNo);
-    	  if (mediaNo == 0) file = mediaService.insertProfileMedia(file);
-    	  else file = mediaService.updateProfileMedia(file);
+        int mediaNo = Integer.parseInt(map.get("mediaNo").toString());
+        file.setMediaNo(mediaNo);
+        if (mediaNo == 0)
+          file = mediaService.insertProfileMedia(file);
+        else
+          file = mediaService.updateProfileMedia(file);
       } else {
-          int postNo = Integer.parseInt(map.get("postNo").toString());
-    	  file.setPostNo(postNo);
-    	  if (postNo == 0) file = mediaService.insertPostMedia(file);
-    	  else file = mediaService.updatePostMedia(file);
+        int postNo = Integer.parseInt(map.get("postNo").toString());
+        file.setPostNo(postNo);
+        if (postNo == 0)
+          file = mediaService.insertPostMedia(file);
+        else
+          file = mediaService.updatePostMedia(file);
       }
       log.info("[uploadImg] file : {}", file);
     } catch (Exception e) {
-        log.info("[uploadImg] e : {}", e);
-        return ResponseEntity.internalServerError().body(new ErrorResponse("이미지 업로드가 실패 하였습니다."));
+      log.info("[uploadImg] e : {}", e);
+      return ResponseEntity.internalServerError().body(new ErrorResponse("이미지 업로드가 실패 하였습니다."));
     }
     return ResponseEntity.ok().body(new CommonResponse<FileVO>(file));
   }
-  
+
   @CrossOrigin("*")
   @RequestMapping(value = "uploadImgs", method = RequestMethod.POST)
-  public ResponseEntity<? extends BasicResponse> requestUploadImageList(@RequestParam("fileList") List<MultipartFile> fileList) {
+  public ResponseEntity<? extends BasicResponse> requestUploadImageList(
+      @RequestParam("fileList") List<MultipartFile> fileList) {
     List<FileVO> resultFileList = null;
     try {
       resultFileList = commonService.saveSvrFileDataAll(fileList, "");
     } catch (Exception e) {
-        return ResponseEntity.internalServerError().body(new ErrorResponse("이미지 업로드가 실패 하였습니다."));
+      return ResponseEntity.internalServerError().body(new ErrorResponse("이미지 업로드가 실패 하였습니다."));
     }
     return ResponseEntity.ok().body(new CommonResponse<List<FileVO>>(resultFileList));
   }
